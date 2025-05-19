2e95c22905723ff250c93bb8ae0460cdcaaf92d5
Chris <chrisbalele21@gmail.com>
2025-05-19 18:44:01 +0300

    a lil update

--- models/auth.go
+++ models/auth.go
@@ -1,12 +1,12 @@
 package models
 
 type RegisterRequest struct {
-	Name     string `json:"name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6"`
+	Name     string `json:"name" binding:"required" example:"Somebody Someone"`
+	Email    string `json:"email" binding:"required,email" example:"somebody@someone.com"`
+	Password string `json:"password" binding:"required,min=6" example:"password123"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email" example:"somebody@someone.com"`
+	Password string `json:"password" binding:"required" example:"password123"`
 }
